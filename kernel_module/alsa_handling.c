e8bf94f7cb3a8d821159a424f07af041b50720a6
Alan Johnston <alan.b.johnston@gmail.com>
2025-10-30 11:02:51 -0400

    Update alsa_handling.c add stdlib.h

--- kernel_module/alsa_handling.c
+++ kernel_module/alsa_handling.c
@@ -14,7 +14,7 @@
 #include <sound/pcm.h>
 #include <sound/initval.h>
 //#include <sound/memalloc.h>
-#include <malloc.h>
+#include <stdlib.h>
 
 #include "alsa_handling.h"
 #include "iq_sample_generation.h"
